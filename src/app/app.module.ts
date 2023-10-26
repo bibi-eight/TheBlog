daff075a7263b8d743602398dc3804fb13e9b13a
Brenda Brito <bbritob8@gmail.com>
2023-10-26 20:53:59 -0300

    incluindo menu title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
