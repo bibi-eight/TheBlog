4f68f38085a224f57630328b0c41ddaebb993884
brenda <bbritob8@gmail.com>
2023-11-13 15:42:40 -0300

    incluindo rota e dados fake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "oi",
+        "title": "ola",
+        "description": "saiu a noticia",
+    },
+    {
+        "id": 2,
+        "photo": "oi",
+        "title": "ola",
+        "description": "saiu a noticia",
+    }
+]
