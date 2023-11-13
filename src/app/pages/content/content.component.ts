3e94b17d9118e348aa41c76472b3e99497537bc3
brenda <bbritob8@gmail.com>
2023-11-13 15:42:21 -0300

    deixando propriedades dinamicas

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  photoCover: string = "https://super.abril.com.br/wp-content/uploads/2020/08/14-08_livros_SITE.jpg?quality=90&strip=info&w=1024&h=682&crop=1"
+  contentTitle: string = "exemplo"
+  contentDescription: string = "bla bla bla bla"
+
   constructor() { }
 
   ngOnInit(): void {
