0a8d367bf5669101ac0886cb3641ddc67dba710f
brenda <bbritob8@gmail.com>
2023-11-14 14:14:14 -0300

    ajustes conteudo

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,17 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "oi",
-        "title": "ola",
-        "description": "saiu a noticia",
+        "id": "1",
+        "title": "Doaê",
+        "description": "Este foi um projeto desenvolvido em grupo para o trabalho final do curso Estartando devs. Nossa ideia era criar uma plataforma que" +
+        "conectasse os doadores com instituições de caridade de forma mais fácil e acessível até para aqueles que não tem tanta prática com redes sociais. Doaê tem o design" +
+        "colorido e chamativo mas também pratico e intuítivo, de forma a facilitar o caminho de ambos os usuários - doador e receptor - na plataforma. Esse projeto foi desenvolvido"+
+        "com javascript e seu frontend foi modelado com react.",
+        "photoCover": "/assets/midia/doaeheader.jpeg"
     },
     {
-        "id": 2,
-        "photo": "oi",
+        "id": "2",
         "title": "ola",
         "description": "saiu a noticia",
+        "photoCover": "/assets/midia/WO.jpg"
     }
 ]
