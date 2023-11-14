568a61fd708b49b41676801051cc8711643f973b
brenda <bbritob8@gmail.com>
2023-11-14 17:14:09 -0300

    ajuste texto e incluindo dados

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,14 +2,27 @@ export const dataFake = [
     {
         "id": "1",
         "title": "Doaê",
-        "description": "Este foi um projeto desenvolvido em grupo para o trabalho final do curso Estartando devs. Nossa ideia era criar uma plataforma que" +
-        "conectasse os doadores com instituições de caridade de forma mais fácil e acessível até para aqueles que não tem tanta prática com redes sociais. Doaê tem o design" +
-        "colorido e chamativo mas também pratico e intuítivo, de forma a facilitar o caminho de ambos os usuários - doador e receptor - na plataforma. Esse projeto foi desenvolvido"+
-        "com javascript e seu frontend foi modelado com react.",
+        "description": "Este foi um projeto desenvolvido em grupo para o trabalho final do curso Estartando devs. Nossa ideia era criar uma plataforma que " +
+        "conectasse os doadores com instituições de caridade de forma mais fácil e acessível até para aqueles que não tem tanta prática com redes sociais. Doaê tem o design " +
+        "colorido e chamativo mas também pratico e intuítivo, de forma a facilitar o caminho de ambos os usuários - doador e receptor - na plataforma. Esse projeto foi desenvolvido "+
+        " com javascript e seu frontend foi modelado com react.",
         "photoCover": "/assets/midia/doaeheader.jpeg"
     },
     {
         "id": "2",
+        "title": "ola",
+        "description": "saiu a noticia",
+        "photoCover": "/assets/midia/WO.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Personal Card",
+        "description": "Essa foi uma tarefa do curso Estartando devs, estavamos aprendendo como funciona o NPM. Como tarefa fizemos um cartão de visita, com nosso nome, idade, " +
+        "Email pra contato e o link do github, e então postamos no NPM. Para baixar o meu, basta escrever npx bibi-eight no terminal e meu cartão de visita vai aparecer. ",
+        "photoCover": "/assets/midia/personalcard.png"
+    }, 
+    {
+        "id": "4",
         "title": "ola",
         "description": "saiu a noticia",
         "photoCover": "/assets/midia/WO.jpg"
